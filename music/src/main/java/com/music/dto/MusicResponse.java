3aabc680c0ec99310debd50d0c01ebcd54e48dd6
seokjin <wlsdldos00@gmail.com>
2024-11-21 23:10:21 +0900

    feat: Create MusicUploadRequest and MusicResponse
    - 음원 업로드 요청 dto 생성
    - 음원 응답 dto 생성

---
+++ music/src/main/java/com/music/dto/MusicResponse.java
@@ -0,0 +1,40 @@
+package com.music.dto;
+
+import com.music.eneity.Music;
+import com.music.eneity.constants.Genre;
+import com.music.eneity.constants.ReleaseStatus;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MusicResponse {
+  private Long id;
+  private Long albumId;
+  private String musicFileKey;
+  private String lyricsFileKey;
+  private String title;
+  private Integer trackNumber;
+  private Integer duration;
+  private LocalDate releaseAt;
+  private Genre genre;
+  private Boolean titleTrack;
+  private ReleaseStatus releaseStatus;
+
+  public static MusicResponse from(Music music) {
+    return MusicResponse.builder()
+        .id(music.getId())
+        .albumId(music.getAlbum().getId())
+        .musicFileKey(music.getMusicFileKey())
+        .lyricsFileKey(music.getLyricsFileKey())
+        .title(music.getTitle())
+        .trackNumber(music.getTrackNumber())
+        .duration(music.getDuration())
+        .releaseAt(music.getReleaseAt())
+        .genre(music.getGenre())
+        .titleTrack(music.getTitleTrack())
+        .releaseStatus(music.getReleaseStatus())
+        .build();
+  }
+}
---
+++ music/src/main/java/com/music/dto/MusicUploadRequest.java
@@ -0,0 +1,36 @@
+package com.music.dto;
+
+import com.music.eneity.constants.Genre;
+import java.time.LocalDate;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class MusicUploadRequest {
+  @NotNull(message = "앨범 아이디는 필수입니다.")
+  private Long albumId;
+
+  @NotBlank(message = "제목은 필수입니다.")
+  private String title;
+
+  @NotNull(message = "트랙 번호는 필수입니다.")
+  @Min(value = 1, message = "트랙 번호는 1 이상이어야 합니다.")
+  private Integer trackNumber;
+
+  @NotNull(message = "재생 시간은 필수입니다.")
+  @Min(value = 1, message = "재생 시간은 1초 이상이어야 합니다.")
+  private Integer duration;
+
+  @NotNull(message = "발매일은 필수입니다.")
+  @Future(message = "발매일은 미래여야 합니다.")
+  private LocalDate releaseAt;
+
+  @NotNull(message = "장르는 필수입니다.")
+  private Genre genre;
+
+  @NotNull(message = "타이틀 곡 여부는 필수입니다.")
+  private Boolean titleTrack;
+}
