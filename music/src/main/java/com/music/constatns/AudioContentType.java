f983c898c2487465baa207f1d66825feac98a332
seokjin <wlsdldos00@gmail.com>
2024-11-23 01:23:29 +0900

    feat: create AudioContentType
    - 오디오 컨텐츠 타입을 정의하고 관련 util 메서드를 구현한 enum 생성

---
+++ music/src/main/java/com/music/constatns/AudioContentType.java
@@ -0,0 +1,45 @@
+package com.music.constatns;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AudioContentType {
+  // AAC 5분 곡 기준 ~10MB로 예상
+  AAC("audio/aac", "aac", 1024 * 1024 * 20L), // 20MB
+  // mp3 10분 곡 기준 ~25MB로 예상
+  MP3("audio/mpeg", "mp3", 1024 * 1024 * 30L), // 30MB
+  // flac 10분 곡 기준 ~40MB로 예상
+  FLAC("audio/flac", "flac", 1024 * 1024 * 50L); // 50MB
+
+  private final String mimeType;
+  private final String extension;
+  private final Long maxSize;
+
+  // 현재 지원하는 타입만 설정
+  private static final Set<AudioContentType> SUPPORTED_TYPES = EnumSet.of(AAC, MP3, FLAC);
+
+  // key: mimeType, value: AudioContentType Enum
+  private static final Map<String, AudioContentType> MIME_TYPE_MAP =
+      Arrays.stream(values())
+          .collect(Collectors.toMap(AudioContentType::getMimeType, t -> t));
+
+  public static boolean isSupportedAudioType(String mimeType) {
+    return Optional.ofNullable(MIME_TYPE_MAP.get(mimeType))
+        .map(SUPPORTED_TYPES::contains)
+        .orElse(false);
+  }
+
+  public static Long getMaxSize(String mimeType) {
+    return Optional.ofNullable(MIME_TYPE_MAP.get(mimeType))
+        .map(AudioContentType::getMaxSize)
+        .orElse(0L);
+  }
+}
