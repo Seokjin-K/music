1d0b719169e6c99a4873927e6c07d90a17d988f8
seokjin <wlsdldos00@gmail.com>
2024-11-24 01:24:25 +0900

    feat: FileStorageService에 @Service 어노테이션 추가

--- music/src/main/java/com/music/service/FileStorageService.java
+++ music/src/main/java/com/music/service/FileStorageService.java
@@ -1,8 +1,10 @@
 package com.music.service;
 
 import com.music.constatns.FileType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public interface FileStorageService {
 
   String uploadFile(MultipartFile file, FileType fileType);
