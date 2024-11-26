35d1efbb394480ad87d67a9b5760160875dd72e8
seokjin <wlsdldos00@gmail.com>
2024-11-27 03:30:46 +0900

    feat: 가사 RDBMS에도 업로드 되도록 구현
    - RDBMS에도 저장하는 로직 구현
    - Lyrcis 엔티티 구현
    - Lyrics의 포맷을 정의하는 LyricesFormat Enum 구현
    - LyricsRepository 구현

---
+++ core/src/main/java/com/music/eneity/Lyrics.java
@@ -0,0 +1,40 @@
+package com.music.eneity;
+
+import com.music.eneity.constants.LyricsFormat;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Lyrics {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "music_id", nullable = false, unique = true)
+  private Music music;
+
+  @Column(nullable = false)
+  private String lyricsFileKey;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private LyricsFormat format;
+}
