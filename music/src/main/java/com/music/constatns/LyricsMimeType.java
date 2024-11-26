45fd00ac0493f46e6779758f6f119e3ea2289ed6
seokjin <wlsdldos00@gmail.com>
2024-11-27 03:05:02 +0900

    rename: LyricsContentType에서 LyricsMimeType으로 변경

---
+++ music/src/main/java/com/music/constatns/LyricsMimeType.java
@@ -0,0 +1,44 @@
+package com.music.constatns;
+
+import com.music.eneity.constants.LyricsFormat;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+@RequiredArgsConstructor
+public enum LyricsMimeType {
+
+  LRC(LyricsFormat.LRC, "application/x-lrc", "lrc", 1024 * 100L), // 100KB
+  TEXT(LyricsFormat.TEXT, "text/plain", "txt", 1024 * 30L); // 30KB
+
+  private static final Logger log = LoggerFactory.getLogger(LyricsMimeType.class);
+  private final LyricsFormat format;
+  private final String mimeType;
+  private final String extension;
+  private final Long maxSize;
+
+  // key: mimeType, value: LyricsContentType
+  private static final Map<String, LyricsMimeType> MIME_TYPE_MAP =
+      Arrays.stream(values())
+          .collect(Collectors.toMap(LyricsMimeType::getMimeType, t -> t));
+
+  public static boolean isSupportedLyricsType(String mimeType) {
+    return MIME_TYPE_MAP.containsKey(mimeType);
+  }
+
+  public static Long getMaxSize(String mimeType) {
+    LyricsMimeType lyricsContentType = MIME_TYPE_MAP.getOrDefault(mimeType, null);
+    return lyricsContentType != null ? lyricsContentType.getMaxSize() : 0L;
+  }
+
+  public static LyricsFormat getFormatByContentType(String mimeType) {
+    return Optional.ofNullable(MIME_TYPE_MAP.get(mimeType).getFormat())
+        .orElseThrow(RuntimeException::new);
+  }
+}
--- music/src/main/java/com/music/validator/LyricFileValidator.java
+++ music/src/main/java/com/music/validator/LyricFileValidator.java
@@ -1,6 +1,6 @@
 package com.music.validator;
 
-import com.music.constatns.LyricContentType;
+import com.music.constatns.LyricsMimeType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,11 +10,11 @@ public class LyricFileValidator implements FileValidator {
   @Override
   public void validate(MultipartFile file) {
     String contentType = file.getContentType();
-    if (!LyricContentType.isSupportedLyricsType(contentType)) { // 지원하는 가사 타입인지 검증
+    if (!LyricsMimeType.isSupportedLyricsType(contentType)) { // 지원하는 가사 타입인지 검증
       log.error("지원하는 가사 파일 형식이 아닙니다. {}", contentType);
       throw new RuntimeException(); // TODO: CustomException 으로 변경
     }
-    if (file.getSize() > LyricContentType.getMaxSize(contentType)) { // 지원하는 파일 크기 인지 검증
+    if (file.getSize() > LyricsMimeType.getMaxSize(contentType)) { // 지원하는 파일 크기 인지 검증
       log.error("파일 크기가 너무 크거나 지원하는 유형이 아닙니다. {}", contentType);
       throw new RuntimeException(); // TODO: CustomException 으로 변경
     }
