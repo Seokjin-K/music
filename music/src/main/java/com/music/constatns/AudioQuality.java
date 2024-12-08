d5ff6a64dcd86bd370e9bd7f25a2fe68b0368a62
seokjin <wlsdldos00@gmail.com>
2024-12-08 15:42:23 +0900

    feat: 퀄리티를 기준으로 파일키를 가져오도록 AudioQuality enum 기능 확장
    - 퀄리티와 파일키를 가져오는 메서드를 매핑한 FILE_KEY_MAPPER 선언
    - 해당 퀄리티의 파일키를 가져오는 getFIleKey 메서드 구현

--- music/src/main/java/com/music/constatns/AudioQuality.java
+++ music/src/main/java/com/music/constatns/AudioQuality.java
@@ -1,5 +1,8 @@
 package com.music.constatns;
 
+import com.music.eneity.Music;
+import java.util.Map;
+import java.util.function.Function;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +18,16 @@ public enum AudioQuality {
   private final int bitrate; // kbps : 1초당 1000비트를 보낼 수 있는 전송 속도
   private final int channels;
   private final int sampleRate;
+
+  private static final Map<AudioQuality, Function<Music, String>> FILE_KEY_MAPPER = Map.of(
+      HIGH, Music::getHighQualityFileKey,
+      MEDIUM, Music::getMediumQualityFileKey,
+      LOW, Music::getLowQualityFileKey
+  );
+
+  public String getFileKey(Music music) {
+    return FILE_KEY_MAPPER.get(this).apply(music);
+  }
 
   public static AudioQuality from(String networkQuality) {
     try {
