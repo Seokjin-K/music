50100d98e77af9fff4a4f6a7f8b3a4b729bc8a64
seokjin <wlsdldos00@gmail.com>
2024-12-12 22:10:18 +0900

    feat: 음원 스트리밍이 끝났을 때의 로그 정보를 담아 반환하는 StreamingEndResponse 구현

---
+++ statistics/src/main/java/com/music/logger/dto/StreamingEndResponse.java
@@ -0,0 +1,35 @@
+package com.music.logger.dto;
+
+import com.music.eneity.StreamingLog;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class StreamingEndResponse {
+
+  private String id;
+  private Long musicId;
+  private String sessionId;
+  private LocalDateTime startTime;
+  private LocalDateTime endTime;
+  private Integer totalDuration;
+  private Integer playedDuration;
+  private Double playedRatio;
+  private LocalDateTime createdAt;
+  // TODO : Add Expire After Day
+
+  public static StreamingEndResponse from(StreamingLog streamingLog) {
+    return StreamingEndResponse.builder()
+        .id(streamingLog.getId())
+        .musicId(streamingLog.getMusicId())
+        .sessionId(streamingLog.getSessionId())
+        .startTime(streamingLog.getStartTime())
+        .endTime(streamingLog.getEndTime())
+        .totalDuration(streamingLog.getTotalDuration())
+        .playedDuration(streamingLog.getPlayedDuration())
+        .playedRatio(streamingLog.getPlayedRatio())
+        .build();
+  }
+}
