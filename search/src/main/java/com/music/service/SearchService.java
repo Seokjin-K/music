48fa40c11d476fa620821459de635a4005dc699c
seokjin <wlsdldos00@gmail.com>
2024-12-15 03:47:58 +0900

    feat: SearchService 메서드 구현 수정 - BoolQuery 생성 수정 및 메서드 분리 - 하이라이팅 제거 - 검색 결과에서 SearchResult를 담지 않고 IntegratedSearch를 담도록 변경

--- search/src/main/java/com/music/service/SearchService.java
+++ search/src/main/java/com/music/service/SearchService.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.music.constants.SortDirection;
 import com.music.constants.SortType;
 import com.music.document.IntegratedSearch;
-import com.music.dto.SearchResult;
 import com.music.dto.IntegratedSearchResponse;
 import com.music.util.SortUtils;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -24,8 +22,6 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -48,41 +44,20 @@ public class SearchService {
       SearchRequest searchRequest = new SearchRequest("integrated_search");
       // 검색 쿼리 구성을 위한 빌더
       SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+      BoolQueryBuilder boolQuery = createBoolQueryBuilder(keyword);
 
-      // Bool 쿼리 생성 - name, content 필드 검색
-      BoolQueryBuilder boolQuery = QueryBuilders.boolQuery()
-          // 이름 필드에 가중치 부여
-          // 음원 관련
-          .should(QueryBuilders.matchQuery("musicTitle", keyword).boost(3.0f))
-          .should(QueryBuilders.matchQuery("lyrics", keyword))
-          // 아티스트 관련
-          .should(QueryBuilders.matchQuery("artistName", keyword).boost(2.5f))
-          .should(QueryBuilders.matchQuery("artistDescription", keyword))
-          // 앨범 관련
-          .should(QueryBuilders.matchQuery("albumTitle", keyword).boost(2.5f))
-          .should(QueryBuilders.matchQuery("albumDescription", keyword))
-          // 공통
-          .should(QueryBuilders.matchQuery("genre", keyword))
-          .minimumShouldMatch(1); // 최소 하나의 조건은 만족해야 함
-
-      // 페이징 처리
-      searchSourceBuilder.query(boolQuery)
-          .from(pageable.getPageNumber() * pageable.getPageSize())
-          .size(pageable.getPageSize());
-
-      // 정렬 설정
-      searchSourceBuilder.sort(SortUtils.getSortBuilder(sortType, sortDirection));
+      configurePaging(pageable, searchSourceBuilder, boolQuery); // 페이징 처리
+      searchSourceBuilder.sort(SortUtils.getSortBuilder(sortType, sortDirection)); // 정렬 설정
       searchRequest.source(searchSourceBuilder);
 
       // 검색 실행
       SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-      List<SearchResult> documents = convertSearchHits(searchResponse.getHits());
+      List<IntegratedSearch> documents = convertSearchHits(searchResponse.getHits());
 
       long totalCount = Optional.ofNullable(searchResponse.getHits().getTotalHits())
           .map(totalHits -> totalHits.value)
           .orElse(0L);
 
-      // 결과 변환 및 반환
       return IntegratedSearchResponse.of(documents, totalCount, pageable);
 
     } catch (IOException e) {
@@ -91,24 +66,38 @@ public class SearchService {
     }
   }
 
-  // 검색 결과 변환 메소드
-  private List<SearchResult> convertSearchHits(SearchHits hits) {
-    return Arrays.stream(hits.getHits())
-        .map(hit -> {
-          // JSON to Document. Elasticsearch 는 JSON 형태로 데이터를 저장/반환.
-          IntegratedSearch document = objectMapper.convertValue(
-              hit.getSourceAsMap(),
-              IntegratedSearch.class
-          );
+  private static void configurePaging(Pageable pageable, SearchSourceBuilder searchSourceBuilder,
+      BoolQueryBuilder boolQuery) {
+    searchSourceBuilder.query(boolQuery)
+        .from(pageable.getPageNumber() * pageable.getPageSize())
+        .size(pageable.getPageSize());
+  }
 
-          // 하이라이팅 처리
-          Map<String, HighlightField> highlightFields = hit.getHighlightFields();
-          String highlightedName = null;
-          if (highlightFields.containsKey("name")) {
-            highlightedName = highlightFields.get("name").getFragments()[0].string();
-          }
-          return SearchResult.of(document, highlightedName);
-        })
+  private static BoolQueryBuilder createBoolQueryBuilder(String keyword) {
+    return QueryBuilders.boolQuery()
+        // 정확한 매칭 쿼리
+        .should(QueryBuilders.termQuery("music_title.keyword", keyword).boost(3.0f))
+        .should(QueryBuilders.termQuery("artist_name.keyword", keyword).boost(2.5f))
+        .should(QueryBuilders.termQuery("album_title.keyword", keyword).boost(2.5f))
+
+        // 부분 매칭 쿼리
+        .should(QueryBuilders.matchQuery("music_title", keyword).boost(3.0f))
+        .should(QueryBuilders.matchQuery("artist_name", keyword).boost(2.5f))
+        .should(QueryBuilders.matchQuery("artist_description", keyword))
+        .should(QueryBuilders.matchQuery("album_title", keyword).boost(2.5f))
+        .should(QueryBuilders.matchQuery("album_description", keyword))
+        // 최소 하나의 조건은 만족해야 함
+        .minimumShouldMatch(1);
+  }
+
+  // 검색 결과 변환 메소드
+  private List<IntegratedSearch> convertSearchHits(SearchHits hits) {
+    return Arrays.stream(hits.getHits())
+        // JSON to Document. Elasticsearch 는 JSON 형태로 데이터를 저장/반환.
+        .map(hit -> objectMapper.convertValue(
+            hit.getSourceAsMap(),
+            IntegratedSearch.class
+        ))
         .collect(Collectors.toList());
   }
 }
