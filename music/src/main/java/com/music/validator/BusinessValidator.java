59eea3fc59bee39b78adab04524ce3ec74254098
seokjin <wlsdldos00@gmail.com>
2024-12-15 21:19:44 +0900

    feat: musilFile과 lyricsFile의 비즈니스 규칙을 검증하는 validateTrackFile 메서드 구현

--- music/src/main/java/com/music/validator/BusinessValidator.java
+++ music/src/main/java/com/music/validator/BusinessValidator.java
@@ -1,10 +1,24 @@
 package com.music.validator;
 
+import com.music.constatns.FileType;
 import com.music.eneity.constants.ReleaseStatus;
 import java.util.EnumSet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
+@RequiredArgsConstructor
 public class BusinessValidator {
+
+  private final FileValidatorFactory fileValidatorFactory;
+
+  public void validateTrackFile(MultipartFile musicFile, MultipartFile lyricFile){
+    if (hasLyricsFile(lyricFile)) {
+      fileValidatorFactory.getValidator(lyricFile, FileType.LYRICS);
+    }
+    fileValidatorFactory.getValidator(musicFile, FileType.MUSIC);
+  }
 
   public static boolean hasLyricsFile(MultipartFile lyricFile) {
     return lyricFile != null && !lyricFile.isEmpty();
