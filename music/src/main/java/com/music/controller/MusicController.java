44e8ce7643195841ed24c7cc30c771d03f40ee12
seokjin <wlsdldos00@gmail.com>
2024-12-15 17:29:04 +0900

    feat: MusicController에 Get 메서드 구현

--- music/src/main/java/com/music/controller/MusicController.java
+++ music/src/main/java/com/music/controller/MusicController.java
@@ -1,11 +1,13 @@
 package com.music.controller;
 
-import com.music.upload.dto.MusicResponse;
+import com.music.upload.dto.MusicUploadResponse;
 import com.music.upload.dto.MusicUploadRequest;
 import com.music.upload.service.MusicUploadService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -15,15 +17,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/music")
-public class MusicUploadController {
+public class MusicController {
 
   private final MusicUploadService musicService;
 
   @PostMapping("/upload")
-  public ResponseEntity<MusicResponse> musicUpload(
+  public ResponseEntity<MusicUploadResponse> musicUpload(
       @Valid @RequestPart MusicUploadRequest request,
       @RequestPart MultipartFile musicFile,
       @RequestPart(required = false) MultipartFile lyricFile) {
     return ResponseEntity.ok(musicService.uploadMusic(request, musicFile, lyricFile));
+  }
+
+  @GetMapping
+  public ResponseEntity<MusicUploadResponse> getMusic() {
+    return ResponseEntity.ok(musicService.getMusic());
   }
 }
