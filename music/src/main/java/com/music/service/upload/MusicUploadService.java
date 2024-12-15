7c5e693f73b5a949925f0ef02aa54c9bc5540237
seokjin <wlsdldos00@gmail.com>
2024-12-15 21:18:53 +0900

    feat: 음원 업로드 요청할 때 Duration을 입력했던 방식을 파일 자체에서 추출하도록 변경
    - 음원 파일과 가사 파일 검증 로직을 BusinessValidator 클래스로 분리
    - procssAudioFileConversion의 이름을 getAudioFileInfo로 변경
    - getAudioFileInfo 메서드에서 트랙을 업로드하고, Filekeys, Duration을 래핑하여 반환하도록 수정
    - createTrackUpload 메서드에서 MusicUpload에 Duration 정보를 포함하도록 수정
    - createMusic 메서드에서 매개변수로 AudioFileInfo를 받도록 수정

--- music/src/main/java/com/music/service/upload/MusicUploadService.java
+++ music/src/main/java/com/music/service/upload/MusicUploadService.java
@@ -1,9 +1,9 @@
 package com.music.service.upload;
 
 import com.music.constatns.AudioQuality;
-import com.music.constatns.FileType;
 import com.music.constatns.LyricsMimeType;
 import com.music.convert.AudioQualityConverter;
+import com.music.dto.upload.AudioFileInfo;
 import com.music.dto.upload.MusicUpload;
 import com.music.dto.upload.FileKeys;
 import com.music.dto.upload.TrackUpload;
@@ -20,7 +20,6 @@ import com.music.repository.MusicRepository;
 import com.music.adaptor.FileStorage;
 import com.music.util.FileUtils;
 import com.music.validator.BusinessValidator;
-import com.music.validator.FileValidatorFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
@@ -40,7 +39,7 @@ public class MusicUploadService {
   private final AlbumRepository albumRepository;
   private final MusicRepository musicRepository;
   private final LyricsRepository lyricsRepository;
-  private final FileValidatorFactory fileValidatorFactory;
+  private final BusinessValidator businessValidator;
   private final AudioQualityConverter audioQualityConverter;
   private final FileStorage fileStorage;
 
@@ -48,44 +47,47 @@ public class MusicUploadService {
   //@Scheduled(cron = "*/10 * * * * *") // 10초마다 실행
   @Scheduled(cron = "0 0 18 * * *") // 오후 6시 정각마다 실행
   public void releaseMusics() {
-    int updateCount = musicRepository
-        .updatePendingToReleasedByBeforeNow(ReleaseStatus.PENDING, ReleaseStatus.RELEASED);
+    long updateCount = musicRepository.updatePendingToReleasedByBeforeNow(
+        ReleaseStatus.PENDING,
+        ReleaseStatus.RELEASED
+    );
     log.info("PENDING -> RELEASED Update {} queries", updateCount);
   }
 
   @Transactional
   public MusicUploadResponse uploadMusic(
-      MusicUploadRequest request, MultipartFile musicFile, MultipartFile lyricFile) {
-
+      MusicUploadRequest request,
+      MultipartFile musicFile,
+      MultipartFile lyricFile
+  ) {
     Album album = getAlbum(request.getAlbumId());
 
-    if (BusinessValidator.hasLyricsFile(lyricFile)) {
-      fileValidatorFactory.getValidator(lyricFile, FileType.LYRICS);
-    }
-    fileValidatorFactory.getValidator(musicFile, FileType.MUSIC);
+    businessValidator.validateTrackFile(musicFile, lyricFile); // 검증
+    AudioFileInfo audioFileInfo = getAudioFileInfo(musicFile, lyricFile); // FileKeys and Duration
 
-    FileKeys fileKeys = processAudioFileConversion(musicFile, lyricFile);
-
-    Music music = createMusic(request, album, fileKeys);
+    Music music = createMusic(request, album, audioFileInfo);
     musicRepository.save(music);
 
-    final String lyricsKey = fileKeys.getLyricsKey();
+    final FileKeys filekeys = audioFileInfo.getFileKeys();
 
-    if (BusinessValidator.hasLyricsFile(lyricFile)) {
-      Lyrics lyrics = createLyrics(lyricFile, music, fileKeys);
-      lyricsRepository.save(lyrics);
-    }
+    uploadLyricsIfExist(lyricFile, music, filekeys);
 
-    return MusicUploadResponse.from(music, lyricsKey);
+    return MusicUploadResponse.from(
+        music,
+        filekeys.getHighQualityKey(),
+        filekeys.getLyricsKey()
+    );
   }
 
-  private FileKeys processAudioFileConversion(
+  private AudioFileInfo getAudioFileInfo(
       MultipartFile musicFile,
       @Nullable MultipartFile lyricsFile
   ) {
     try {
       TrackUpload trackUpload = createTrackUpload(musicFile, lyricsFile);
-      return fileStorage.uploadTrack(trackUpload);
+      FileKeys fileKeys = fileStorage.uploadTrack(trackUpload);
+
+      return AudioFileInfo.of(fileKeys, trackUpload.getDuration());
     } catch (IOException e) {
       log.error("오디오 파일 프로세싱 실패. {}", e.getMessage());
       throw new RuntimeException(e); // TODO: CustomException 으로 변경 필요
@@ -96,17 +98,22 @@ public class MusicUploadService {
       MultipartFile musicFile,
       @Nullable MultipartFile lyricsFile
   ) throws IOException {
-
     Map<AudioQuality, File> convertedMusicFiles = audioQualityConverter.process(musicFile);
-    MusicUpload musicUpload = MusicUpload.of(musicFile, convertedMusicFiles);
 
+    int duration = audioQualityConverter.extractDuration(
+        convertedMusicFiles.get(AudioQuality.HIGH)
+    );
+
+    MusicUpload musicUpload = MusicUpload.of(musicFile, duration, convertedMusicFiles);
     LyricsUpload lyricsUpload = createLyricsUploadIfExists(lyricsFile);
+
     return TrackUpload.of(musicUpload, lyricsUpload);
   }
 
   private static LyricsUpload createLyricsUploadIfExists(MultipartFile lyricsFile)
       throws IOException {
     LyricsUpload lyricsUpload = null;
+
     if (BusinessValidator.hasLyricsFile(lyricsFile)) {
       File convertedLyricsFile = FileUtils.convertMultipartFileToFile(lyricsFile);
       lyricsUpload = LyricsUpload.of(lyricsFile, convertedLyricsFile);
@@ -116,6 +123,7 @@ public class MusicUploadService {
 
   @Transactional(readOnly = true)
   private Album getAlbum(Long albumId) {
+
     Album album = albumRepository.findById(albumId)
         .orElseThrow(RuntimeException::new); // TODO: CustomException 으로 변경 필요
 
@@ -125,17 +133,18 @@ public class MusicUploadService {
 
   private Music createMusic(
       MusicUploadRequest request,
-      Album album, FileKeys
-      musicFileKeys
+      Album album,
+      AudioFileInfo audioFileInfo
   ) {
+    FileKeys fileKeys = audioFileInfo.getFileKeys();
     return Music.builder()
         .album(album)
-        .highQualityFileKey(musicFileKeys.getHighQualityKey())
-        .mediumQualityFileKey(musicFileKeys.getMediumQualityKey())
-        .lowQualityFileKey(musicFileKeys.getLowQualityKey())
+        .highQualityFileKey(fileKeys.getHighQualityKey())
+        .mediumQualityFileKey(fileKeys.getMediumQualityKey())
+        .lowQualityFileKey(fileKeys.getLowQualityKey())
         .title(request.getTitle())
         .trackNumber(request.getTrackNumber())
-        .duration(request.getDuration())
+        .duration(audioFileInfo.getDuration())
         .releaseAt(request.getReleaseAt())
         .genre(request.getGenre())
         .titleTrack(request.getTitleTrack())
@@ -143,14 +152,21 @@ public class MusicUploadService {
         .build();
   }
 
+  private void uploadLyricsIfExist(MultipartFile lyricFile, Music music, FileKeys filekeys) {
+    if (BusinessValidator.hasLyricsFile(lyricFile)) {
+      Lyrics lyrics = createLyrics(lyricFile, music, filekeys.getLyricsKey());
+      lyricsRepository.save(lyrics);
+    }
+  }
+
   private static Lyrics createLyrics(
       MultipartFile lyricFile,
       Music music,
-      FileKeys fileKeys
+      String lyricsKey
   ) {
     return Lyrics.builder()
         .music(music)
-        .lyricsFileKey(fileKeys.getLyricsKey())
+        .lyricsFileKey(lyricsKey)
         .lyricsFormat(LyricsMimeType.getFormatByContentType(lyricFile.getContentType()))
         .build();
   }
