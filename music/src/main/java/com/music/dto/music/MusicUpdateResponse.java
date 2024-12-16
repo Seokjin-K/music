7944cc06caf5c35f0266547b8fb80cc69b338aec
seokjin <wlsdldos00@gmail.com>
2024-12-16 15:37:46 +0900

    feat: Music 및 Lyrics 업데이트 요청값과 반환값을 래핑하기 위한 dto 구현

---
+++ music/src/main/java/com/music/dto/music/MusicUpdateResponse.java
@@ -0,0 +1,56 @@
+package com.music.dto.music;
+
+import com.music.eneity.Lyrics;
+import com.music.eneity.Music;
+import com.music.eneity.constants.Genre;
+import com.music.eneity.constants.ReleaseStatus;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Builder
+public class MusicUpdateResponse {
+
+  private Long musicId;
+
+  private Long lyricsId;
+
+  private String highQualityFileKey;
+
+  private String mediumQualityFileKey;
+
+  private String lowQualityFileKey;
+
+  private String title;
+
+  private Integer trackNumber;
+
+  private Integer duration;
+
+  private LocalDate releaseAt;
+
+  private Genre genre;
+
+  private Boolean titleTrack;
+
+  private ReleaseStatus releaseStatus;
+
+  public static MusicUpdateResponse of(Music music, @Nullable Lyrics lyrics) {
+    return MusicUpdateResponse.builder()
+        .musicId(music.getId())
+        .lyricsId(lyrics != null ? lyrics.getId() : null)
+        .highQualityFileKey(music.getHighQualityFileKey())
+        .mediumQualityFileKey(music.getMediumQualityFileKey())
+        .lowQualityFileKey(music.getLowQualityFileKey())
+        .title(music.getTitle())
+        .trackNumber(music.getTrackNumber())
+        .duration(music.getDuration())
+        .releaseAt(music.getReleaseAt())
+        .genre(music.getGenre())
+        .titleTrack(music.getTitleTrack())
+        .releaseStatus(music.getReleaseStatus())
+        .build();
+  }
+}
