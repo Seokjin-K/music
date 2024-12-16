c09a1418107c794b0f45e8a3c5aadbd8f8581158
seokjin <wlsdldos00@gmail.com>
2024-12-16 15:27:08 +0900

    feat: FileStorage 인터페이스에 메서드 추가 정의
    - 음원 파일들만 업로드하는 uplodaMusic 메서드 정의
    - 가사 파일만 업로드하는 uplodaLyrics 메서드 정의
    - 다중 파일을 제거하는 deleteFiles 메서드 정의
    - 단일 파일을 제거하는 deleteFile 메서드 정의

--- music/src/main/java/com/music/adaptor/FileStorage.java
+++ music/src/main/java/com/music/adaptor/FileStorage.java
@@ -1,12 +1,21 @@
 package com.music.adaptor;
 
 import com.music.dto.upload.FileKeys;
+import com.music.dto.upload.LyricsUpload;
+import com.music.dto.upload.MusicUpload;
 import com.music.dto.upload.TrackUpload;
+import com.music.eneity.Lyrics;
 import java.io.InputStream;
 
 public interface FileStorage {
 
   FileKeys uploadTrack(TrackUpload filesUploadRequest);
 
+  FileKeys uploadMusic(MusicUpload musicUpload);
+  String uploadLyrics(LyricsUpload lyricsUpload);
+
   InputStream getFileStream(String musicFileKey);
+
+  void deleteFiles(FileKeys fileKeys);
+  void deleteFile(String fileKey);
 }
