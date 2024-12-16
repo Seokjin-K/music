eacca51a5c49b79725a477e30cb67c152100b53f
seokjin <wlsdldos00@gmail.com>
2024-12-16 16:24:24 +0900

    feat: 음원 삭제 기능을 담당할 MusicDeleteService 클래스 구현

---
+++ music/src/main/java/com/music/service/music/MusicDeleteService.java
@@ -0,0 +1,22 @@
+package com.music.service.music;
+
+import com.music.eneity.constants.ReleaseStatus;
+import com.music.repository.MusicRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MusicDeleteService {
+
+  private final MusicRepository musicRepository;
+
+  @Transactional
+  public Long deleteMusic(Long musicId) {
+    int deleteCount = musicRepository.updateMusicStatus(musicId, ReleaseStatus.DELETED);
+    return deleteCount > 0 ? musicId : -1;
+  }
+}
