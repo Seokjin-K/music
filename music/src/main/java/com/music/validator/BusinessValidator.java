d444a4f8f21d366b1159d24841bcba4783147056
seokjin <wlsdldos00@gmail.com>
2024-12-16 15:36:28 +0900

    feat: 하나의 파일 정보만 검증하는 validateFile 메서드 구현

--- music/src/main/java/com/music/validator/BusinessValidator.java
+++ music/src/main/java/com/music/validator/BusinessValidator.java
@@ -4,20 +4,26 @@ import com.music.constatns.FileType;
 import com.music.eneity.constants.ReleaseStatus;
 import java.util.EnumSet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BusinessValidator {
 
   private final FileValidatorFactory fileValidatorFactory;
 
-  public void validateTrackFile(MultipartFile musicFile, MultipartFile lyricFile){
+  public void validateTrackFile(MultipartFile musicFile, MultipartFile lyricFile) {
     if (hasLyricsFile(lyricFile)) {
       fileValidatorFactory.getValidator(lyricFile, FileType.LYRICS);
     }
     fileValidatorFactory.getValidator(musicFile, FileType.MUSIC);
+  }
+
+  public void validateFile(MultipartFile file, FileType fileType) {
+    fileValidatorFactory.getValidator(file, fileType);
   }
 
   public static boolean hasLyricsFile(MultipartFile lyricFile) {
@@ -26,6 +32,7 @@ public class BusinessValidator {
 
   public static void validateReleaseStatus(ReleaseStatus status) {
     if (!EnumSet.of(ReleaseStatus.PENDING, ReleaseStatus.RELEASED).contains(status)) {
+      log.error("ReleaseStatus invalid : {}", status);
       throw new RuntimeException(); // TODO: CustomException 으로 변경 필요
     }
   }
